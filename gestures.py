826d434229cdab03dc3ed4f1208ddc271201417a
austin sanchez <austinsanchez45@yahoo.com>
2021-10-19 14:04:54 -0500

    Created a new file that will replace the list of gestures.

---
+++ gestures.py
@@ -0,0 +1,19 @@
+
+
+class Gestures:
+    def __init__(self, name, gesture_loses_to):
+        self.name = ""
+        self.gesture_loses_to = []
+
+
+    def gesture_choices(self, name, gesture_loses_to):
+        if self.name == "Rock":
+            self.gesture_loses_to = ["Paper", "Spock"]
+        elif self.name == "Paper":
+            self.gesture_loses_to = ["Scissors", "Lizard"]
+        elif self.name == "Scissors":
+            self.gesture_loses_to = ["Rock", "Spock"]
+        elif self.name == "Lizard":
+            self.gesture_loses_to = ["Rock", "Scissors"]
+        elif self.name == "Spock":
+            self.gesture_loses_to = ["Paper", "Lizard"]
